d49b05b73428d49bfc070b2190382c91fe1cb026
Abhijit Pal <abhijitpal121@gmail.com>
2026-06-01 19:42:32 +0530

    Design issue fixed

--- resources/views/trip/show-v2.blade.php
+++ resources/views/trip/show-v2.blade.php
@@ -2,7 +2,7 @@
 
 @section('css')
 <link href="{{ asset('css/fleet/vehicle-details-v2.css?v=5.6') }}" rel="stylesheet">
-<link href="{{ asset('css/trip/show-v2.css?v=4.2') }}" rel="stylesheet">
+<link href="{{ asset('css/trip/show-v2.css?v=4.4') }}" rel="stylesheet">
 @endsection
 
 @section('content')
