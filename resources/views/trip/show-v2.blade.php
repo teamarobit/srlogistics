9f4c1fd61a453839ecba79c66a25f94cefe8e91d
Abhijit Pal <abhijitpal121@gmail.com>
2026-06-01 18:48:22 +0530

    Button related design fixed

--- resources/views/trip/show-v2.blade.php
+++ resources/views/trip/show-v2.blade.php
@@ -2,7 +2,7 @@
 
 @section('css')
 <link href="{{ asset('css/fleet/vehicle-details-v2.css?v=5.6') }}" rel="stylesheet">
-<link href="{{ asset('css/trip/show-v2.css?v=4.1') }}" rel="stylesheet">
+<link href="{{ asset('css/trip/show-v2.css?v=4.2') }}" rel="stylesheet">
 @endsection
 
 @section('content')
