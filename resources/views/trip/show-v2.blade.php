5ebc8e9778bd88feb9af6fe43a66eaa702ba8825
Abhijit Pal <abhijitpal121@gmail.com>
2026-06-03 12:05:03 +0530

    Vehicle Details Modal design compact

--- resources/views/trip/show-v2.blade.php
+++ resources/views/trip/show-v2.blade.php
@@ -2,7 +2,7 @@
 
 @section('css')
 <link href="{{ asset('css/fleet/vehicle-details-v2.css?v=5.6') }}" rel="stylesheet">
-<link href="{{ asset('css/trip/show-v2.css?v=6.1') }}" rel="stylesheet">
+<link href="{{ asset('css/trip/show-v2.css?v=6.3') }}" rel="stylesheet">
 @endsection
 
 @section('content')
