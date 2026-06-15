df13c9bf36a455bde69c2dc922a79e28ba15251d
Abhijit Pal <abhijitpal121@gmail.com>
2026-06-15 13:35:37 +0530

    Tyre Vendor tax issue solved

--- resources/views/contacts/vehiclevendor/edit.blade.php
+++ resources/views/contacts/vehiclevendor/edit.blade.php
@@ -777,7 +777,10 @@
                                                 <small class="error text-danger atyperr" id="edit_coattachtype_0_error"></small>
                                             </div>
                                         </div>
-                                        
+
+                                        {{-- TDS Declaration rule message (shown when TDS % is 0 or 1 and the document is missing) --}}
+                                        <small class="error text-danger d-block mt-1" id="edit_attachtypes_error"></small>
+
                                         <hr>
                                         
                                         <!--dropzone-->
